d5602be5367e5efd7f1064f2fb830a6e575b305d
GANESH27B <bommidiganesh27@gmail.com>
2025-06-24 22:41:37 +0530

    Delete src directory

--- src/services/authService.js
+++
@@ -1,9 +0,0 @@
-// src/services/authService.js
-import axios from 'axios';
-
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
-export const login = async (email, password) => {
-  const res = await axios.post(`${API_URL}/api/auth/login`, { email, password });
-  return res.data;
-};
